70e1599d8c883414ebe4edf75da800b82eb67ad2
Alvaro Flores <asflum@proton.me>
2025-07-15 10:57:45 -0500

    fix: add web client id

--- fronted/app/src/main/java/com/asflum/cashewregister/CredentialProvider.kt
+++ fronted/app/src/main/java/com/asflum/cashewregister/CredentialProvider.kt
@@ -6,18 +6,20 @@ import androidx.credentials.CredentialManager
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 
 object CredentialProvider {
+    const val WEB_CLIENT_ID = BuildConfig.WEB_CLIENT_ID
+    fun getCredentialManager(context: Context): CredentialManager {
+        return CredentialManager.create(context)
+    }
+
     fun createCredentialRequest(): GetCredentialRequest {
         val googleIdOption = GetGoogleIdOption.Builder()
             .setFilterByAuthorizedAccounts(false)
+            .setServerClientId(WEB_CLIENT_ID)
             .build()
 
         return GetCredentialRequest.Builder()
             .addCredentialOption(googleIdOption)
             .build()
-    }
-
-    fun getCredentialManager(context: Context): CredentialManager {
-        return CredentialManager.create(context)
     }
 }
 
