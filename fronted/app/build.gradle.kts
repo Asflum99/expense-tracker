080ecb834879f2fbce1557b1990345c127544052
Alvaro Flores <asflum@proton.me>
2025-07-15 12:10:50 -0500

    update targetSdk to 36

--- fronted/app/build.gradle.kts
+++ fronted/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.asflum.cashewregister"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1"
 
